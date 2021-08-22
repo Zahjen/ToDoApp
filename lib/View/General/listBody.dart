e172d7c2cb89d0bb02898b05e5d4a64a1d5826ff
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-08-22 03:36:34 +0200

    Add files via upload

---
+++ lib/View/General/listBody.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:my_agenda/Models/Pojo/group.dart';
+import 'package:my_agenda/Models/Style/someStyle.dart';
+import 'package:my_agenda/View/List/addList.dart';
+import 'package:my_agenda/View/List/completeListContainer.dart';
+import 'package:my_agenda/View/List/todayList.dart';
+import 'package:my_agenda/View/List/uncompleteListContainer.dart';
+
+class ListBodyPart extends StatefulWidget {
+  final Group group;
+  
+  const ListBodyPart({ 
+    Key? key,
+    required this.group
+  }) : super(key: key);
+
+  @override
+  _ListBodyPartState createState() => _ListBodyPartState();
+}
+
+class _ListBodyPartState extends State<ListBodyPart> {
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<Widget> optionPage = [
+      UncompleteListContainer(group: widget.group),
+      CompleteListContainer(group: widget.group),
+      TodayList(group: widget.group)
+    ];
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: paddingContainer,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AddList(group: widget.group),
+              SizedBox(
+                height: 20.0,
+              ),
+              optionPage[selectedIndex],
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list
+              ),
+              label: '',
+              tooltip: 'Uncomplete',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.fact_check_outlined
+              ),
+              label: '',
+              tooltip: 'Complete'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.calendar_today
+              ),
+              label: '',
+              tooltip: 'Today'
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: onItemTapped,
+        ),
+      )
+    );
+  }
+}
