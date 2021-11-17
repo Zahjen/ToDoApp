947fe5123602b9b8e44f6651d883f0831034ca62
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 08:50:34 +0100

    Update completeSlidableTask.dart

--- lib/View/Task/completeSlidableTask.dart
+++ lib/View/Task/completeSlidableTask.dart
@@ -24,46 +24,50 @@ class _CompleteSlidableTaskState extends State<CompleteSlidableTask>
     super.build(context);
 
     return StreamBuilder<List<Task>>(
-        stream: NoSqlTask().getAllTrue(widget.list),
-        builder: (BuildContext context, AsyncSnapshot<List<Task>> snap) {
-          if (snap.connectionState == ConnectionState.waiting) {
-            return Text('Loading...');
-          }
-          if (!snap.hasData) {
-            return Text("Query error");
-          }
-          if (snap.data!.isEmpty) {
-            return Text("No task here yet!");
-          }
+      stream: NoSqlTask().getAllTrue(widget.list),
+      builder: (BuildContext context, AsyncSnapshot<List<Task>> snap) {
+        if (snap.connectionState == ConnectionState.waiting) {
+          return Text('Loading...');
+        }
+        if (!snap.hasData) {
+          return Text("Query error");
+        }
+        if (snap.data!.isEmpty) {
+          return Text("No task here yet!");
+        }
 
-          return ListView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: snap.data?.length,
-              itemBuilder: (BuildContext context, int index) {
-                Task task = snap.data![index];
+        return ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: snap.data?.length,
+          itemBuilder: (BuildContext context, int index) {
+            Task task = snap.data![index];
 
-                return SlidableContainer(
-                    child: TaskContainer(task: task),
-                    controller: slidableController,
-                    builderEdit: (BuildContext context) {
-                      initializeEditTask(task);
-                      return DialogTask(
-                          actionToDo: 'Edit',
-                          setData: () {
-                            editDataTask(context, task);
-                          });
-                    },
-                    builderDelete: (BuildContext context) {
-                      return DeleteConfirmationDialog(
-                          deleteThing: 'this task',
-                          deleteConfirmation: () {
-                            NoSqlTask().delete(task);
-                            Navigator.pop(context);
-                          });
+            return SlidableContainer(
+              child: TaskContainer(task: task),
+              controller: slidableController,
+              builderEdit: (BuildContext context) {
+                initializeEditTask(task);
+                return DialogTask(
+                    actionToDo: 'Edit',
+                    setData: () {
+                      editDataTask(context, task);
                     });
-              });
-        });
+              },
+              builderDelete: (BuildContext context) {
+                return DeleteConfirmationDialog(
+                  deleteThing: 'this task',
+                  deleteConfirmation: () {
+                    NoSqlTask().delete(task);
+                    Navigator.pop(context);
+                  }
+                );
+              }
+            );
+          }
+        );
+      }
+    );
   }
 
   @override
