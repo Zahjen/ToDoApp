1b7740eea0f69651fdd276c73aa994a773822317
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 08:54:08 +0100

    Update taskContainer.dart

--- lib/View/Task/taskContainer.dart
+++ lib/View/Task/taskContainer.dart
@@ -1,66 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:my_agenda/Models/Database/noSqlTask.dart';
-import 'package:my_agenda/Models/Pojo/task.dart';
-import 'package:my_agenda/Models/Style/someStyle.dart';
-import 'package:my_agenda/Models/Utils/utils.dart';
+import '../../Models/Database/noSqlTask.dart';
+import '../../Models/Pojo/task.dart';
+import '../../Models/Style/someStyle.dart';
+import '../../Models/Utils/utils.dart';
 
 class TaskContainer extends StatelessWidget {
   final Task task;
-  
-  const TaskContainer({ 
-    Key? key, 
-    required this.task 
-  }) : super(key: key);
+
+  const TaskContainer({Key? key, required this.task}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: paddingContainerCard,
       decoration: containerBox,
-      child:Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
-            flex: 7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(task.getTitle()),
-                SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  task.getDescription(),
-                  style: taskDescriptionStyle,
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  Utils.getDate(task.getDeadline()),
-                  style: taskDeadlineStyle,
-                )
-              ],
-            )
-          ),
+              flex: 7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(task.getTitle()),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    task.getDescription(),
+                    style: task.getDescription() != ""
+                        ? taskDescriptionStyle
+                        : TextStyle(fontSize: 0),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    Utils.getDate(task.getDeadline()),
+                    style: taskDeadlineStyle,
+                  )
+                ],
+              )),
           Expanded(
             flex: 3,
             child: Align(
               alignment: Alignment.centerRight,
               child: IconButton(
-                onPressed: (){
+                onPressed: () {
                   NoSqlTask().updateState(task);
-                }, 
-                icon: (task.getState() == true) ?
-                  Icon(
-                    Icons.circle
-                  ) : 
-                  Icon(
-                    Icons.circle_outlined
-                  )
+                },
+                icon: (task.getState() == true)
+                    ? Icon(Icons.circle)
+                    : Icon(Icons.circle_outlined)
               )
             )
-          )  
+          )
         ],
       )
     );
