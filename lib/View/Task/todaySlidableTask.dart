240e7f29db2fecbd086e01bdfed64f28d4f9d507
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 08:49:01 +0100

    Update todaySlidableTask.dart

--- lib/View/Task/todaySlidableTask.dart
+++ lib/View/Task/todaySlidableTask.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:my_agenda/Models/Database/noSqlTask.dart';
-import 'package:my_agenda/Models/Pojo/list.dart';
-import 'package:my_agenda/Models/Pojo/task.dart';
-import 'package:my_agenda/Models/Widgets/deleteConfirmDialog.dart';
-import 'package:my_agenda/Models/Widgets/dialogTask.dart';
-import 'package:my_agenda/Models/Widgets/slidableContainer.dart';
-import 'package:my_agenda/View/Task/taskContainer.dart';
-import 'package:my_agenda/Controller/controllerTask.dart';
+import '../../Models/Database/noSqlTask.dart';
+import '../../Models/Pojo/list.dart';
+import '../../Models/Pojo/task.dart';
+import '../../Models/Widgets/deleteConfirmDialog.dart';
+import '../../Models/Widgets/dialogTask.dart';
+import '../../Models/Widgets/slidableContainer.dart';
+import '../../View/Task/taskContainer.dart';
+import '../../Controller/controllerTask.dart';
 
-class TodaySlidableTask extends StatelessWidget {
+class TodaySlidableTask extends StatefulWidget {
   final Liste list;
 
-  const TodaySlidableTask({ 
-    Key? key, 
-    required this.list 
-  }) : super(key: key);
+  const TodaySlidableTask({Key? key, required this.list}) : super(key: key);
 
   @override
+  State<TodaySlidableTask> createState() => _TodaySlidableTaskState();
+}
+
+class _TodaySlidableTaskState extends State<TodaySlidableTask>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     return StreamBuilder<List<Task>>(
-      stream: NoSqlTask().getAllForToday(list),
+      stream: NoSqlTask().getAllForToday(widget.list),
       builder: (BuildContext context, AsyncSnapshot<List<Task>> snap) {
         if (snap.connectionState == ConnectionState.waiting) {
           return Text('Loading...');
@@ -36,24 +41,22 @@ class TodaySlidableTask extends StatelessWidget {
           shrinkWrap: true,
           itemCount: snap.data?.length,
           itemBuilder: (BuildContext context, int index) {
-      
             Task task = snap.data![index];
 
             return SlidableContainer(
-              child: TaskContainer(task: task), 
-              controller: slidableController, 
+              child: TaskContainer(task: task),
+              controller: slidableController,
               builderEdit: (BuildContext context) {
                 initializeEditTask(task);
                 return DialogTask(
-                  actionToDo: 'Edit', 
-                  setData: () {
-                    editDataTask(context, task);
-                  }
-                );
-              }, 
+                    actionToDo: 'Edit',
+                    setData: () {
+                      editDataTask(context, task);
+                    });
+              },
               builderDelete: (BuildContext context) {
                 return DeleteConfirmationDialog(
-                  deleteThing: 'this task', 
+                  deleteThing: 'this task',
                   deleteConfirmation: () {
                     NoSqlTask().delete(task);
                     Navigator.pop(context);
@@ -66,5 +69,7 @@ class TodaySlidableTask extends StatelessWidget {
       }
     );
   }
-}
 
+  @override
+  bool get wantKeepAlive => true;
+}
