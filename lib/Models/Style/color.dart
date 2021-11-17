09f784cd9656a956765b8451ddf2fcac270e14d3
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 09:29:58 +0100

    Update color.dart

--- lib/Models/Style/color.dart
+++ lib/Models/Style/color.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Palette { 
-  static const MaterialColor colorApp = const MaterialColor( 
-    0xFFffb3c9, 
-    const <int, Color>{ 
-      50: const Color(0xFF2d2d3f),
+class Palette {
+  static const MaterialColor colorApp = const MaterialColor(
+    0xFFFFCAB5,
+    const <int, Color>{
+      50: const Color(0xFF1f2933),
       100: const Color(0xFFe9e9e9),
-      200: const Color(0xff1f1f2e),
+      200: const Color(0xFF323f48),
       300: const Color(0xff999999),
       400: const Color(0xFFFF3838),
       500: const Color(0xffbcbcbc),
-      600: const Color(0xff16161f),  
-      700: const Color(0xff0e0e1c),
-      
+      600: const Color(0xFF405461),
+      700: const Color(0xFF000000),
       800: const Color(0xff170907),
       900: const Color(0xff000000),
-    }, 
-  ); 
+    },
+  );
 }
