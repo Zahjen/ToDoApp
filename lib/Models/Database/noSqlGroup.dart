e8762b630227836d9223f3a60e45b28b53ef7b53
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 09:33:21 +0100

    Update noSqlGroup.dart

--- lib/Models/Database/noSqlGroup.dart
+++ lib/Models/Database/noSqlGroup.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:my_agenda/Models/Database/noSqlList.dart';
-import 'package:my_agenda/Models/Pojo/group.dart';
-import 'package:my_agenda/Models/Pojo/list.dart';
+import '../../Models/Database/noSqlList.dart';
+import '../../Models/Pojo/group.dart';
+import '../../Models/Pojo/list.dart';
 
 class NoSqlGroup {
 
