e1c8fdd0f0fbde74e4241b672a797ac42013bec1
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 09:26:32 +0100

    Update dialogGroupList.dart

--- lib/Models/Widgets/dialogGroupList.dart
+++ lib/Models/Widgets/dialogGroupList.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_agenda/Models/Style/someStyle.dart';
+import '../../Models/Style/someStyle.dart';
 
 class DialogGroupList extends StatefulWidget {
   final String title;
