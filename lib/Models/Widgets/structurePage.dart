a0aee5e5c13ace8e481871eb01ec034d4e04fd6d
Zahjen <77575832+Zahjen@users.noreply.github.com>
2021-11-17 09:23:36 +0100

    Update structurePage.dart

--- lib/Models/Widgets/structurePage.dart
+++ lib/Models/Widgets/structurePage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_agenda/Models/Style/someStyle.dart';
+import '../../Models/Style/someStyle.dart';
 
 class StructurePage extends StatelessWidget {
   final List<Widget> children;
